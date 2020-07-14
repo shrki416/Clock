cc5371974c418cc3757718e0c5125ce39a01875e
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-07-14 17:16:56 -0400

    Add am/pm

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 const today = new Date();
 const day = today.getDay();
-const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const dayOfTheWeekd = document.getElementById("day").innerHTML = weekDays[day];
-const month = document.getElementById("month").innerHTML = today.toLocaleDateString(`default`, { month: 'long' });
-const date = document.getElementById("date").innerHTML = today.getDate();
-const year = document.getElementById("year").innerHTML = today.getFullYear();
+const toggle = document.getElementById("toggle-btn");
+let militaryTime = false;
+
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const dayOfTheWeekd = (document.getElementById("day").innerHTML =
+  weekDays[day]);
+const month = (document.getElementById(
+  "month"
+).innerHTML = today.toLocaleDateString(`default`, { month: "long" }));
+const date = (document.getElementById("date").innerHTML = today.getDate());
+const year = (document.getElementById("year").innerHTML = today.getFullYear());
 
 function clock() {
-    const today = new Date();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
+  const today = new Date();
+  const currentTime = document.getElementById("time");
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
 
-    // let am_pm = hours >= 12 ? 'pm' : 'am'; hours = hours % 12;
-    // hours = hours ? hours : 12;
-    // minutes = minutes < 10 ? '0' + minutes : minutes;
+  let amPM = hours < 12 ? "am" : "pm";
 
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
+  if (militaryTime === true) {
+    hours = hours > 12 ? hours - 12 : hours;
+    toggle.textContent = "Standard";
+  }
 
-    document.getElementById('hr').innerHTML = hours;
-    document.getElementById('min').innerHTML = minutes;
-    document.getElementById('sec').innerHTML = seconds;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  currentTime.innerHTML = `${hours}:${minutes}:${seconds} ${amPM}`;
 }
 
-setInterval(clock, 100);
+toggle.addEventListener("click", () => {
+  militaryTime = !militaryTime;
+  toggle.textContent = "Military";
+});
 
-let am_pm = today.toLocaleTimeString(); // local time in am/pm format
-console.log(am_pm);
+setInterval(clock, 1000);
